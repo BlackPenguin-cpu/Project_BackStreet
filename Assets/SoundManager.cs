76da3f778effd6a735c59c8407352c85e187c5e5
Black_Penguin <dkstmdwnswns@gmail.com>
2021-12-06 20:46:19 +0900

    매니저 수정중

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[System.Serializable]
+public class Clip
+{
+    public string Name;
+    public AudioClip clip;
+}
+public class SoundManager : Singleton<SoundManager>
+{
+    public AudioSource audioSource;
+    public List<Clip> clips;
+
+    float SEvolume = 1;
+    protected SoundManager() { }
+    private void Start()
+    {
+        //clips.Sort();
+    }
+
+    public void Playbgm(string name)
+    //사용법 Sound.Instance.ChangeClip("이름",루프 할껀지안할껀지(bool))
+    {
+        Clip find = clips.Find((o) => { return o.Name == name; });
+        if (find != null)
+        {
+            audioSource.Stop();
+            audioSource.clip = find.clip;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+
+    public void PlaySound(string _clip)
+    {
+        Clip find = clips.Find((o) => { return o.Name == _clip; });
+        if (find != null)
+        {
+            GameObject audio_object = new GameObject();
+            AudioSource object_source = audio_object.AddComponent<AudioSource>();
+            object_source.volume = SEvolume;
+            object_source.clip = find.clip;
+            object_source.loop = false;
+            object_source.Play();
+
+            Destroy(audio_object, find.clip.length);
+        }
+    }
+    public void PlaySound(AudioClip _clip)
+    {
+        GameObject audio_object = new GameObject();
+        AudioSource object_source = audio_object.AddComponent<AudioSource>();
+        object_source.volume = SEvolume;
+        object_source.clip = _clip;
+        object_source.loop = false;
+        object_source.Play();
+
+        Destroy(audio_object, _clip.length);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioSource.volume = volume;
+    }
+    public void SetSEVolume(float volume)
+    {
+        SEvolume = volume;
+    }
+}
